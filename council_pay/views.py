055be00c8122b046546fe5c1f5415922343934c7
andrewtrench <andrew.trench@media24.com>
2012-03-14 14:29:30 +0200

    second commit

--- council_pay/views.py
+++ council_pay/views.py
@@ -9,3 +9,36 @@ from myproject.council_pay.models import Councilpay
 def home(request):
    all_councils = Councilpay.objects.all().order_by('council')[:50]
    return render_to_response('index.html',{'all_councils':all_councils})
+   
+def search_form(request):
+	
+    return render_to_response('search_form.html')   
+
+def council_results(council_list):
+		all_councils=council_list
+		return render_to_response('index.html', {'all_councils': all_councils})
+	
+def search(request):
+    if 'q' in request.GET:
+		term = (request.GET['q']).title()
+		search = Councilpay.objects.filter(council__icontains=term)
+		
+		council_list =[]
+		roles = []
+		data = []
+		for item in search:
+		   
+		   if item.council not in council_list:
+		      council_list.append(item.council)
+		print len(council_list)	  
+		print council_list
+		if len (council_list) ==1:
+
+		   return render_to_response('search.html', {'search':search})
+                if len(council_list)>1:
+			all_councils = Councilpay.objects.filter(council__in=council_list)
+			return render_to_response('index.html', {'all_councils':all_councils})
+			
+    else:
+        message = 'You submitted an empty form.'
+        return HttpResponse(message)
