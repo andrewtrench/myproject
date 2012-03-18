d6f74e6f8f720a502ed85d509be8e4453c8cdbca
andrewtrench <andrew.trench@media24.com>
2012-03-18 20:54:00 +0200

    unday night

---
+++ council_pay/templates/graph_prototype.py
@@ -0,0 +1,24 @@
+https://github.com/gak/pygooglechart/blob/master/examples/bar.py
+
+from pygooglechart import StackedHorizontalBarChart, StackedVerticalBarChart, \
+    GroupedHorizontalBarChart, GroupedVerticalBarChart
+
+	
+	new_chart = StackedHorizontalBarChart(800,600)
+>>> data_list=[]
+>>> for item in data:
+	if item == '':
+		item = 0
+	data_list.append(item)
+
+	
+>>> labels = []
+>>> labels = data_dict.keys()
+>>> new_chart.set_axis_labels('y', labels)
+0
+>>> data_list=[]
+>>> for item in data:
+	if item == '':
+		item = 0
+	data_list.append(item)
+new_chart.set_colours(['D6F8F8'])
