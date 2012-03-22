c425719a4bf9053069938ded3376f9542f15dc2a
Andrew Trench <andrew.trench@media24.com>
2012-03-22 09:28:42 -0500

    late thurs march 22

--- urls.py
+++ urls.py
@@ -25,7 +25,7 @@ urlpatterns = patterns('',
 	 url(r'^pricey/', 'myproject.council_pay.views.pricey_council'),
 	 url(r'^mayors/', 'myproject.council_pay.views.mayors'),
 	 url(r'^mm/', 'myproject.council_pay.views.mm'),
-	 url(r'^exec/', 'myproject.council_pay.views.total_exec'),
+	 url(r'^executives/', 'myproject.council_pay.views.total_exec'),
 	 
 )
 
